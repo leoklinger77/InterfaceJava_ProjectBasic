088d37eabe82c2f02d09e8136d019c0c1a8965e6
Leandro Klinger <Leandro.klingeroliveira@gmail.com>
2020-07-19 12:18:59 -0300

    OnelinePaymentService class created

--- src/model/entities/Contract.java
+++ src/model/entities/Contract.java
@@ -1,12 +1,16 @@
 package model.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Contract {
 
 	private Integer number;
 	private Date date;
 	private Double totalValue;
+	
+	private List<Installment> list = new ArrayList<>();
 	
 	public Contract(Integer number, Date date, Double totalValue) {		
 		this.number = number;
@@ -24,6 +28,12 @@ public class Contract {
 	}
 	public Double getTotalValue() {
 		return totalValue;
+	}
+	public void addList(Installment instaloment) {
+		list.add(instaloment);
+	}
+	public void removeList(Installment instaloment) {
+		list.remove(instaloment);
 	}
 	
 	
